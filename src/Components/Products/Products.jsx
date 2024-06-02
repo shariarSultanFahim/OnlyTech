79f4a2748d9ce3652c3051ecb0c18c745c80e9ce
Shariar Sultan Fahim <157113723+shariarSultanFahim@users.noreply.github.com>
2024-06-02 11:09:29 +0600

    Added pagination on the all products page

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,15 +1,37 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../AuthProvider/AuthProvider";
 import ProductCard from "../ProductCard/ProductCard";
 
 const Products = () => {
-    const {products} = useContext(AuthContext)
+    const {products} = useContext(AuthContext);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 6;
+    const totalPage = Math.ceil([products?.length / itemsPerPage]);
+    const currentProducts = products?.slice(
+        (currentPage - 1) * itemsPerPage,
+        currentPage * itemsPerPage
+      );
+      const handlePageChange = (page) =>{
+        setCurrentPage(page);
+      }
     return (
-        <div className="my-10 flex flex-wrap gap-6">
-                {
-                    products?.map(product => <ProductCard  key={product._id} product={product}  /> )
-                }
-        </div>
+       <div>
+            <div className="my-10 flex flex-wrap gap-6">
+                    {
+                        currentProducts?.map(product => <ProductCard  key={product._id} product={product}  /> )
+                    }
+            </div>
+            <div className="join my-10 flex justify-center">
+                {Array.from({ length: totalPage }, (_, index) => (
+                <button
+                    key={index + 1}
+                    className={`join-item btn btn-md ${currentPage === index + 1 ? 'btn-active' : ''}`}
+                    onClick={() => handlePageChange(index + 1)}>
+                    {index + 1}
+                </button>
+                ))}
+            </div>
+       </div>
     );
 };
 
